975fc26b707cd9391af07b14369b069f27a71bb2
Kshitij Kumar <kshitij2003raj@gmail.com>
2024-08-17 13:26:22 +0530

    Configure Api invoke url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://21c2evrqme.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
